f3718769636a9dccd58482e910d39836db22953a
Aysha <ayshapp468@gmail.com>
2024-12-03 20:37:11 +0530

    fix: update missing opening tax amount check

--- hrms/payroll/doctype/salary_structure_assignment/salary_structure_assignment.py
+++ hrms/payroll/doctype/salary_structure_assignment/salary_structure_assignment.py
@@ -169,6 +169,10 @@ class SalaryStructureAssignment(Document):
 
 	@frappe.whitelist()
 	def are_opening_entries_required(self) -> bool:
+		tax_component = get_tax_component(self.salary_structure)
+		if not tax_component:
+			return False
+
 		if self.has_emp_joined_after_payroll_period_start() and not self.has_existing_salary_slips():
 			return True
 		else:
