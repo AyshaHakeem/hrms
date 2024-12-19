534435d07418584a036f57176bc348c7a84f8f7a
Aysha <ayshapp468@gmail.com>
2024-12-19 13:04:27 +0530

    feat: allow user to bulk allocate leaves manually

--- hrms/hr/doctype/leave_allocation/leave_allocation_list.js
+++ hrms/hr/doctype/leave_allocation/leave_allocation_list.js
@@ -8,4 +8,43 @@ frappe.listview_settings["Leave Allocation"] = {
 			return [__("Expired"), "gray", "expired, =, 1"];
 		}
 	},
+	onload: function (listview) {
+		listview.page.add_action_item(
+			__("Allocate Leaves"),
+			() => {
+				const allocations = listview
+					.get_checked_items()
+					.map((allocation) => allocation.name);
+				const dialog = new frappe.ui.Dialog({
+					title: "Manual Leave Allocation",
+					fields: [
+						{
+							label: "New Leaves to be Allocated",
+							fieldname: "new_leaves",
+							fieldtype: "Float",
+						},
+					],
+					primary_action_label: "Allocate",
+					primary_action({ new_leaves }) {
+						frappe.call({
+							method: "hrms.hr.doctype.leave_allocation.leave_allocation.bulk_allocate_leaves",
+							args: { allocations, new_leaves },
+							freeze: true,
+							freeze_message: __("Allocating Leaves"),
+						});
+						dialog.hide();
+					},
+				});
+				dialog.show();
+			},
+			__("Actions"),
+		);
+	},
+	refresh: function (listview) {
+		hrms.handle_realtime_bulk_action_notification(
+			listview,
+			"completed_bulk_leave_allocation",
+			"Leave Allocation",
+		);
+	},
 };
